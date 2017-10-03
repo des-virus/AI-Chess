cb3c7256a9ea051a40549a0a184cfeefd9ac28a2
minhphong306 <dominhphong306@gmail.com>
2017-10-03 17:32:28 +0700

    Add check function

--- src/ai/chess/AIChess.java
+++ src/ai/chess/AIChess.java
@@ -11,11 +11,52 @@ package ai.chess;
  */
 public class AIChess {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    int WIDTH = 5;
+    int HEIGHT = 7;
+
+    int a[][] = {{1, 1, 1, 1, 1},
+    {1, 1, 0, 1, 1},
+    {1, 0, 0, 0, 1},
+    {1, 1, 0, 1, 1},
+    {1, 1, 0, 1, 1},
+    {-1, 2, 0, 2, -1},
+    {-1, -1, 2, -1, -1},};
+
+    public boolean CheckLen(int i, int j) {
+        if (i == 0 || a[i - 1][j] != 0) {
+            return false;
+        }
+        return true;
+    }
+
+    public boolean CheckXuong(int i, int j) {
+        if (i == 6 || a[i + 1][j] != 0) {
+            return false;
+        }
+        return true;
+    }
+
+    public boolean CheckTrai(int i, int j) {
+        if (j == 0 || a[i][j - 1] != 0) {
+            return false;
+        }
+        return true;
+    }
+
+    public boolean CheckPhai(int i, int j) {
+        if (j == 4 || a[i][j + 1] != 0) 
+            return false;
+        return true;
+    }
+
+    public boolean CheckCheoChinhLen(int i, int j){
+        if(i == 0 || j == 0 || a[i - 1][j - 1] != 0)
+            return false;
+        return true;
     }
     
+    public static void main(String[] args) {
+
+    }
+
 }
